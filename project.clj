2343c31e8283310c3f103092549218c5a9c951f2
shaolang <shaolang@ymail.com>
2013-12-17 21:20:32 +0800

    Upgrade Leiningen dependency.

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject lein-cucumber "1.0.3-SNAPSHOT"
   :description "Run cucumber-jvm specifications with leiningen"
   :dependencies [[info.cukes/cucumber-clojure "1.1.1"]
-                 [leiningen-core "2.0.0"]]
+                 [leiningen-core "2.3.4"]]
   :profiles {:cucumber {:dependencies [[commons-io "2.0"]]
                         :plugins [[lein-cucumber "1.0.3-SNAPSHOT"]]}}
   :eval-in :leiningen
