08a9d75968b1b354d5285d8777a4adbbb5dc0a9e
shaolang <shaolang@ymail.com>
2014-02-25 20:57:25 +0800

    Added midje dev dependency.

--- project.clj
+++ project.clj
@@ -3,7 +3,9 @@
   :dependencies [[info.cukes/cucumber-clojure "1.1.6-SNAPSHOT"]
                  [leiningen-core "2.3.4"]]
   :profiles {:cucumber {:dependencies [[commons-io "2.0"]]
-                        :plugins [[lein-cucumber "1.0.3-SNAPSHOT"]]}}
+                        :plugins [[lein-cucumber "1.0.3-SNAPSHOT"]]}
+             :dev {:dependencies [[midje "1.6.2"]]
+                   :plugins [[lein-midje "3.1.3"]]}}
   :eval-in :leiningen
   :license {:name "Unlicense"
             :url "http://unlicense.org/"
