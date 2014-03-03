67576fcf199992049a30708750f4eede4c3504be
shaolang <shaolang@ymail.com>
2014-03-03 13:11:41 +0800

    Bumped up major version to mark this complete rewrite.

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject lein-cucumber "1.0.3-SNAPSHOT"
+(defproject lein-cucumber "2.0.0-RC1"
   :description "Run cucumber-jvm specifications with leiningen"
   :dependencies [[info.cukes/cucumber-clojure "1.1.6-SNAPSHOT"]
                  [leiningen-core "2.3.4"]]
   :profiles {:cucumber {:dependencies [[commons-io "2.0"]]
-                        :plugins [[lein-cucumber "1.0.3-SNAPSHOT"]]}
+                        :plugins [[lein-cucumber "2.0.0-RC1"]]}
              :dev {:dependencies [[midje "1.6.2"]]
                    :plugins [[lein-midje "3.1.3"]]}}
   :eval-in :leiningen
