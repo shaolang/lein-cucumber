6fc6dbd6375bc3152e2799324334b56dd43b1c4e
shaolang <shaolang@ymail.com>
2014-03-05 00:18:06 +0800

    Bumped version down to 2.0.0-SNAPSHOT instead.
    There are still some features in 1.x series not ported to this version.
    SNAPSHOT is more appropriate in this case.

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject lein-cucumber "2.0.0-RC1"
+(defproject lein-cucumber "2.0.0-SNAPSHOT"
   :description "Run cucumber-jvm specifications with leiningen"
   :dependencies [[info.cukes/cucumber-clojure "1.1.6-SNAPSHOT"]
                  [leiningen-core "2.3.4"]]
   :profiles {:cucumber {:dependencies [[commons-io "2.0"]]
-                        :plugins [[lein-cucumber "2.0.0-RC1"]]}
+                        :plugins [[lein-cucumber "2.0.0-SNAPSHOT"]]}
              :dev {:dependencies [[midje "1.6.2"]]
                    :plugins [[lein-midje "3.1.3"]]}}
   :eval-in :leiningen
