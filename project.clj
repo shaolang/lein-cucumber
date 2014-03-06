478e00d84bfdd1ea3f6aabee1a3c91a57c4da607
shaolang <shaolang@ymail.com>
2014-03-06 22:06:54 +0800

    Added profiles for multiple versions of cucumber-jvm.

--- project.clj
+++ project.clj
@@ -3,9 +3,13 @@
   :dependencies [[leiningen-core "2.3.4"]]
   :profiles {:cucumber {:dependencies [[commons-io "2.0"]]
                         :plugins [[lein-cucumber "2.0.0-SNAPSHOT"]]}
-             :dev {:dependencies [[info.cukes/cucumber-clojure "1.1.6-SNAPSHOT"]
+             :dev {:dependencies [[info.cukes/cucumber-clojure "1.1.5"]
                                   [midje "1.6.2"]]
                    :plugins [[lein-midje "3.1.3"]]}
+             :1.1.6 {:dependencies [[info.cukes/cucumber-clojure "1.1.6-SNAPSHOT"]]}
+             :1.1.4 {:dependencies [[info.cukes/cucumber-clojure "1.1.4"]]}
+             :1.1.3 {:dependencies [[info.cukes/cucumber-clojure "1.1.3"]]}
+             :1.1.2 {:dependencies [[info.cukes/cucumber-clojure "1.1.2"]]}
              :1.1.1 {:dependencies [[info.cukes/cucumber-clojure "1.1.1"]]}}
   :eval-in :leiningen
   :license {:name "Unlicense"
