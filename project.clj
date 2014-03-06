b9faae13161365cbb5b0df4a84b11277de48bd2d
shaolang <shaolang@ymail.com>
2014-03-06 17:01:18 +0800

    Added profile to test on cucumber-jvm 1.1.1.
    And it works!!!!

--- project.clj
+++ project.clj
@@ -1,11 +1,12 @@
 (defproject lein-cucumber "2.0.0-SNAPSHOT"
   :description "Run cucumber-jvm specifications with leiningen"
-  :dependencies [[info.cukes/cucumber-clojure "1.1.6-SNAPSHOT"]
-                 [leiningen-core "2.3.4"]]
+  :dependencies [[leiningen-core "2.3.4"]]
   :profiles {:cucumber {:dependencies [[commons-io "2.0"]]
                         :plugins [[lein-cucumber "2.0.0-SNAPSHOT"]]}
-             :dev {:dependencies [[midje "1.6.2"]]
-                   :plugins [[lein-midje "3.1.3"]]}}
+             :dev {:dependencies [[info.cukes/cucumber-clojure "1.1.6-SNAPSHOT"]
+                                  [midje "1.6.2"]]
+                   :plugins [[lein-midje "3.1.3"]]}
+             :1.1.1 {:dependencies [[info.cukes/cucumber-clojure "1.1.1"]]}}
   :eval-in :leiningen
   :license {:name "Unlicense"
             :url "http://unlicense.org/"
